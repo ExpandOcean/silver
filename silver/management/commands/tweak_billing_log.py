3b56a5368d775bfef2cc3f38379c7630222122c8
freeyourmind <freeyourmind@126.com>
2018-08-21 16:02:21 +0800

    dev: add PY3 support

--- silver/management/commands/tweak_billing_log.py
+++ silver/management/commands/tweak_billing_log.py
@@ -24,11 +24,6 @@ from silver.models import Subscription, BillingLog
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('--date',
-                    action='store',
-                    dest='date'),
-    )
 
     def handle(self, *args, **options):
         if options['date']:
